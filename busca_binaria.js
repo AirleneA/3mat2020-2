49ee1b0066f6582cf0c582b74f23fbcb7217c1aa
AirleneA <airlene.antonelli@gmail.com>
2020-09-03 12:21:17 +0000

    2020.09.03

---
+++ busca_binaria.js
@@ -0,0 +1,58 @@
+// Busca binária só funciona com uma lista preordenada
+function buscaBinaria(lista, valorBusca) {
+    let inicio = 0
+    let fim = lista.length - 1
+    
+    while(fim >= inicio) {
+        // Math.floor() tira as casas decimais - Math(M maiusculo)
+        let meio = Math.floor((fim + inicio) / 2)
+        
+        if(lista[meio] === valorBusca) {//Achou
+            comp++
+            return meio
+        }
+         else if(valorBusca < lista[meio]){
+            // Trazendo o indicador de fim para uma posição
+            // antes do meio, assim descartando todos os valores
+            // que são maiores que o valor da posição média
+            comp += 2
+            fim = meio - 1 
+        }
+        else { // valor Busca > lista[meio]
+            comp += 2
+            inicio = meio + 1
+        }
+    }
+    return -1  // o valor não foi encontrado
+}
+
+const primos = require [./Dados/primos']
+
+console.time('7723')
+console.log(buscaBinaria(primos, 7723))
+console.timeEnd('7723')
+console.log('comparações: '), comp
+
+console.log('....................................')
+
+comp = 0
+console.time('31')
+console.log(buscaBinaria(primos, 31))
+console.timeEnd('31')
+console.log('comparações: '), comp
+
+console.log('....................................')
+
+comp = 0
+console.time('3359')
+console.log(buscaBinaria(primos, 3359))
+console.timeEnd('3359')
+console.log('comparações: '), comp
+
+console.log('....................................')
+
+comp = 0
+console.time('3010')
+console.log(buscaBinaria(primos, 3010))
+console.timeEnd('3010')
+console.log('comparações: '), comp
---
+++ busca_binaria2.js
@@ -0,0 +1,50 @@
+function buscaBinaria(lista, valorBusca) {
+    let inicio = 0
+    let fim = lista.length - 1
+    
+    while(fim >= inicio) {
+        // Math.floor() tira as casas decimais - Math(M maiusculo)
+        let meio = Math.floor((fim + inicio) / 2)
+    
+        //chamar a função externa de comparação para
+        //direcionar os proximos passos
+        let res = fnComp(lista[meio], valorBusca)
+
+        if(lista[meio] === 0) {//Achou
+            comp++
+            return meio
+        }
+         else if(res < 0){
+            // Trazendo o indicador de fim para uma posição
+            // antes do meio, assim descartando todos os valores
+            // que são maiores que o valor da posição média
+            comp += 2
+            fim = meio - 1 
+        }
+        else { // res > 0]
+            comp += 2
+            inicio = meio + 1
+        }
+    }
+    return -1  // o valor não foi encontrado
+}
+
+const listaNomes = require('./Dados/lista-nomes')
+console.time('FAUSTO')
+console.log(buscaBinaria(listaNomes, 'FAUSTO',(obj, busca) => {
+    if(obj.first_name === busca) return 0
+    else if (busca < obj.first_name) return -1
+    else 1
+}))
+
+console.timeEnd('FAUSTO')
+console.log('comparações: comp')
+
+console.log('.......................')
+
+console.time('FAUSTO')
+console.log(buscaBinaria(listaNomes, 'FAUSTO',(obj, busca) => {
+    if(obj.first_name === busca) return 0
+    else if (busca < obj.first_name) return -1
+    else 1
+}))
